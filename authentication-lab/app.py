d4f693c3e90b0bbf22a1eba1c139d906c218d3d1
yahliru24-meet <93874001+yahliru24-meet@users.noreply.github.com>
2023-07-24 13:08:10 +0300

    :rabbit:

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -22,8 +22,16 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = "Authentication failed"
     return render_template("signin.html")
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -33,7 +41,7 @@ def signup():
         password = request.form['password']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('home'))
+            return redirect(url_for('add_tweet'))
         except:
             error = "Authentication failed"
     return render_template("signup.html")
